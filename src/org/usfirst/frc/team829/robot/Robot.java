6d49aaba3581e21c99a3ec6eaa3a4a1d9ce06c8d
Blake Johnson <blake.k.johnson.4@gmail.com>
2017-03-07 17:39:57 -0500

    Created Robot class base

---
+++ src/org/usfirst/frc/team829/robot/Robot.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team829.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+
+	public void robotInit() {
+	}
+	
+	public void autonomousInit() {
+	}
+	
+	public void autonomousPeriodic() {
+	}
+	
+	public void teleopPeriodic() {
+	}
+	
+	public void testInit() {
+	}
+	
+	public void testPeriodic() {
+	}
+	
+	public void disabledInit() {
+	}
+	
+}
