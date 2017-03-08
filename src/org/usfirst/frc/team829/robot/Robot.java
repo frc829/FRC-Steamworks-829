ecaf47975c489bc422f9fe721015060afa519f17
Blake Johnson <blake.k.johnson.4@gmail.com>
2017-03-07 19:48:20 -0500

    Added user control during Tele-Op

--- src/org/usfirst/frc/team829/robot/Robot.java
+++ src/org/usfirst/frc/team829/robot/Robot.java
@@ -1,13 +1,22 @@
 package org.usfirst.frc.team829.robot;
 
+import org.usfirst.frc.team829.controller.LogitechController;
+import org.usfirst.frc.team829.system.Climb;
+import org.usfirst.frc.team829.system.Drive;
+import org.usfirst.frc.team829.system.Gear;
 import org.usfirst.frc.team829.system.RobotMap;
+import org.usfirst.frc.team829.system.Shooter;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
 
+	LogitechController driver, operator;
+	
 	public void robotInit() {
 		RobotMap.setup();
+		driver = new LogitechController(0);
+		operator = new LogitechController(1);
 	}
 	
 	public void autonomousInit() {
@@ -17,6 +26,52 @@ public class Robot extends IterativeRobot {
 	}
 	
 	public void teleopPeriodic() {
+		
+		// Climb
+		if(driver.getLeftBumper() && driver.getRightBumper())
+			Climb.climbUp();
+		else
+			Climb.setClimbSpeed(0);
+		
+		// Drive
+		Drive.setDriveSpeed(-driver.getAxisValue("lefty"), -driver.getAxisValue("righty"));
+		
+		if(driver.getStartButton())
+			Drive.togglePrecise();
+		
+		// Gear
+		if(operator.getStartButton())
+			Gear.pivotUp();
+		else if(operator.getSelectButton())
+			Gear.pivotDown();
+		else
+			Gear.setGearPivotSpeed(0);
+		
+		if(operator.getAButton())
+			Gear.grabGear();
+		else if(operator.getBButton())
+			Gear.releaseGear();
+		
+		// Shooter
+		if(operator.getLeftBumper() && operator.getRightBumper())
+			Shooter.runShooter();
+		else
+			Shooter.setShooterVoltage(0);
+		
+		if(operator.getAxisValue("lefttrigger") == 1 && operator.getAxisValue("righttrigger") == 1)
+			Shooter.spinSingulator();
+		else
+			Shooter.setShooterSingulatorSpeed(0);
+		
+		if(operator.getXButton())
+			Shooter.spinCentrifuge();
+		else
+			Shooter.setShooterCentrifugeSpeed(0);
+		
+		if(driver.getSelectButton())
+			Shooter.toggleShooterLight();
+		Shooter.updateLight();
+		
 	}
 	
 	public void testInit() {
