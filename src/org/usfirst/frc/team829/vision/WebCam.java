9317b28d32320ac8910871eaf94f9b81255a2023
Blake Johnson <johnsbla000@warren.k12.in.us>
2017-03-14 18:52:42 -0400

    Removed disabling and enabling of camera view

--- src/org/usfirst/frc/team829/vision/WebCam.java
+++ src/org/usfirst/frc/team829/vision/WebCam.java
@@ -1,46 +1,17 @@
 package org.usfirst.frc.team829.vision;
 
-import org.opencv.core.Mat;
-
-import edu.wpi.cscore.CvSink;
-import edu.wpi.cscore.CvSource;
-import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 
 public class WebCam {
-
-	Thread visionThread;
-	UsbCamera camera;
-	CvSource source;
-	CvSink cvSink;
-	Mat mat;
 	
-	public WebCam() {
-		
-		visionThread = new Thread(() -> {
-			
-			camera = CameraServer.getInstance().startAutomaticCapture();
-			camera.setResolution(640, 480);
-			
-			cvSink = CameraServer.getInstance().getVideo();
-			source = CameraServer.getInstance().putVideo("Main Video", 640, 480);
-			
-			while(!Thread.interrupted()) {
-				cvSink.grabFrame(mat);
-				source.putFrame(mat);
-			}
-			
-		});
-		
+	public WebCam() {		
 	}
 	
 	public void startCapture() {
-		visionThread.setDaemon(true);
-		visionThread.start();
+		CameraServer.getInstance().startAutomaticCapture();
 	}
 	
 	public void stopCapture() {
-		visionThread.interrupt();
 	}
 	
 }
